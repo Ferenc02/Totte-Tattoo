ac1349d139a1b2699838cfc70c888d2e0bf6b80f
MatildaSundqvist <matildasundqvist@live.se>
2025-05-15 11:29:00 +0200

    added middleware error handler

--- src/backend/app.mjs
+++ src/backend/app.mjs
@@ -3,8 +3,9 @@ import dotenv from "dotenv";
 import connectDb from './db/db.mjs';
 // import mediaRouter from "./routes/media-routes.mjs";
 // import consultationRouter from "./routes/consultation-routes.mjs";
-import AppError from './models/AppError.mjs';
 import bookingRouter from './routes/booking-routes.mjs';
+import errorHandler from "./middleware/errorHandler.mjs";
+import AppError from "./models/AppError.mjs";
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -18,14 +19,17 @@ app.use("/api/v1/booking", bookingRouter);
 // app.use("/api/v1/consultation", consultationRouter);
 // app.use("/api/v1/media", mediaRouter);
 
-// app.all("*", (req, res, next) => {
-//   next(
-//     new AppError(
-//       `Vi kan tyvärr inte hitta resursen som du söker ${req.originalUrl}`,
-//       404
-//     )
-//   );
-// });
+/*app.all("*", (req, res, next) => {
+
+    next(
+        new AppError(
+            `Vi kan tyvärr inte hitta resursen som du söker, ${req.originalUrl}`, 404
+
+        )
+    );
+});*/
+
+app.use(errorHandler);
 
 
 
