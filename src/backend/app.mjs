5a8f9940a9e3f1ccf08ca2576d4126bc934689f2
MatildaSundqvist <matildasundqvist@live.se>
2025-05-15 11:33:44 +0200

    ta bort gammal import

--- src/backend/app.mjs
+++ src/backend/app.mjs
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import connectDb from './db/db.mjs';
 // import mediaRouter from "./routes/media-routes.mjs";
 import consultationRouter from './routes/consultation-routes.mjs';
-import AppError from './models/AppError.mjs';
+
 import bookingRouter from './routes/booking-routes.mjs';
 import errorHandler from "./middleware/errorHandler.mjs";
 import AppError from "./models/AppError.mjs";
