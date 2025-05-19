88b2dd243f01f7909b55e77f130564324ca23fd3
Hanesj <hannes.sjostrom96@gmail.com>
2025-05-19 17:00:45 +0200

    Booking continue

--- src/backend/controllers/booking-controller.mjs
+++ src/backend/controllers/booking-controller.mjs
@@ -14,6 +14,7 @@ export const addBooking = catchErrorAsync(async (req, res) => {
 export const listOpenTimeSlots = catchErrorAsync(async (req, res) => {
 	const { year, month } = req.body;
 	const slots = await new BookingRepository().listSlots(year, month);
+	console.log(slots);
 	res.status(200).json({ success: true, data: slots });
 });
 
