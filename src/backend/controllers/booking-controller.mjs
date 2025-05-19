8e361e693f60b353a0fff61d7668df227f9e25d3
Manuel <69732401+manu-cadena@users.noreply.github.com>
2025-05-19 13:27:06 +0200

    fix getById

--- src/backend/controllers/booking-controller.mjs
+++ src/backend/controllers/booking-controller.mjs
@@ -1,25 +1,25 @@
-import BookingRepository from "../repositories/BookingRepository.mjs";
-import { catchErrorAsync } from "../utilities/catchErrorAsync.mjs";
+import BookingRepository from '../repositories/BookingRepository.mjs';
+import { catchErrorAsync } from '../utilities/catchErrorAsync.mjs';
 
 export const listAllBookings = catchErrorAsync(async (req, res) => {
   const bookings = await new BookingRepository().listAll();
   res.status(200).json({ success: true, data: bookings });
 });
 
-export const addBooking = catchErrorAsync(async (req,res) => {
+export const addBooking = catchErrorAsync(async (req, res) => {
   const booking = await new BookingRepository().add(req);
   res.status(200).json({ success: true, data: booking });
 });
 
 export const listOpenTimeSlots = catchErrorAsync(async (req, res) => {
-  const {year, month} = req.body;
+  const { year, month } = req.body;
   const slots = await new BookingRepository().listSlots(year, month);
   res.status(200).json({ success: true, data: slots });
 });
 
 export const getBooking = catchErrorAsync(async (req, res) => {
-  const booking = await new BookingRepository().getById(req.params.id);
-  res.status(200).json({success: true, data: booking });
+  const booking = await new BookingRepository().find(req.params.id);
+  res.status(200).json({ success: true, data: booking });
 });
 
 export const updateBooking = catchErrorAsync(async (req, res) => {
@@ -27,9 +27,7 @@ export const updateBooking = catchErrorAsync(async (req, res) => {
   res.status(204).end();
 });
 
-
 export const deleteBooking = catchErrorAsync(async (req, res) => {
   await new BookingRepository().delete(req.params.id);
   res.status(204).end();
 });
-
